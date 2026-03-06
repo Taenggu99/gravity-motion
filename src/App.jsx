3570abe25fcfc0412f800c0e2a52f95d5fc6f0d0
Taenggu99 <leetaeeun99@gmail.com>
2026-03-06 19:14:17 +0900

    build : 초기 셋팅

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Matter from 'matter-js';
+import { Rocket, Globe2, Magnet, PlusCircle, Eraser } from 'lucide-react';
+
+const App = () => {
+
+};
+
+export default App;
